5dff5c6862f50a216db157e77bcc42cf6d10036f
sagd330 <45902409+sagd330@users.noreply.github.com>
2019-06-20 16:20:54 -0500

    Add files via upload

---
+++ ESP2RM_STM8S103_BLYNK/ESP2RM_STM8S103_BLYNK.ino
@@ -0,0 +1,38 @@
+const int ms = 20;
+
+void setup() {    
+  pinMode(2, OUTPUT);
+
+  digitalWrite(2,LOW);
+  Serial.begin(115200, SERIAL_8N1);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  
+        //turn relay 1 on
+        Serial.write(0xA0);
+        Serial.write(0x01);
+        Serial.write(0x01);
+        Serial.write(0xA2);
+        delay(ms);
+        //turn relay 2 on
+        Serial.write(0xA0);
+        Serial.write(0x02);
+        Serial.write(0x01);
+        Serial.write(0xA3);
+        delay(1000);                      // Wait for a second
+        
+        //turn relay 1 off
+        Serial.write(0xA0);
+        Serial.write(0x01);
+        Serial.write(0x00);
+        Serial.write(0xA1);
+        delay(ms); 
+        //turn relay 2 off
+        Serial.write(0xA0);
+        Serial.write(0x02);
+        Serial.write(0x00);
+        Serial.write(0xA2);
+        delay(2000);                      // Wait for two seconds 
+}
